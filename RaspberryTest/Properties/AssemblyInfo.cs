22406784c7ad0b528fbdac599f9b883465bc74dc
Wilco de Jong <wilco@willybilly.nl>
2018-05-13 15:48:54 +0200

    Updated layout

--- RaspberryTest/Properties/AssemblyInfo.cs
+++ RaspberryTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RaspberryTest")]
+[assembly: AssemblyTitle("FoscamViewerUWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RaspberryTest")]
+[assembly: AssemblyCompany("WillyBilly")]
+[assembly: AssemblyProduct("FoscamViewerUWP")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
